d64b5d8d9ec18406667e67a486dac11ff3af6f43
Tobias Hilka <tobias.hilka@nexusgroup.com>
2018-03-26 13:04:43 +0200

    Minor layout tweaks for the list.

--- lib/mainlist/mainlist.dart
+++ lib/mainlist/mainlist.dart
@@ -7,22 +7,29 @@ class MainList extends StatefulWidget {
 
 class MainListState extends State<MainList> {
 
+  final _items = [1, 2, 3, 4, 5];
+
   @override
   Widget build(BuildContext context) {
 
     final list = new ListView.builder(
-      padding: const EdgeInsets.all(16.0),
+      padding: const EdgeInsets.all(2.0),
       itemBuilder: (context, i) {
         if (i.isOdd) return new Divider();
 
         final index = i ~/ 2;
 
-        if (index < 20) {
-          final text = new Text(
-            "$index",
-            style: const TextStyle(fontSize: 18.0),
+        if (index < _items.length) {
+          final int txt = _items[index];
+          final container = new Container(
+            child: new Text(
+              "Item $txt",
+              style: const TextStyle(fontSize: 18.0),
+            ),
+            padding: const EdgeInsets.all(8.0),
           );
-          return text;
+
+          return container;
         }
       },
     );

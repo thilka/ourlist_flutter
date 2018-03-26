8b510ac4b00b76a975d874054eace20b17da6322
Tobias Hilka <tobias.hilka@nexusgroup.com>
2018-03-26 11:53:49 +0200

    adjusted test for Hello World

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,20 +10,13 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:ourlist_flutter/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Hello World smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(new MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
+    // Verify that we display Hello World.
+    expect(find.text('Hello World'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
 }

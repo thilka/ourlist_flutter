0a50639eb5b961b4a0021389981c2c3be80541ff
Tobias Hilka <tobias.hilka@nexusgroup.com>
2018-03-27 12:20:31 +0200

    Implemented inline adding of new items.

--- lib/details/detailslist.dart
+++ lib/details/detailslist.dart
@@ -6,20 +6,55 @@ class DetailsList extends StatefulWidget {
 }
 
 class _DetailsListState extends State<DetailsList> {
+
+  final _tiles = [
+    createListTile("One"),
+    createListTile("Two"),
+  ];
+  static ListTile createListTile(String input) => new ListTile(title: new Text(input));
+
+  var _editMode = false;
+
   @override
   Widget build(BuildContext context) {
-    final tiles = [
-      new ListTile(title: new Text("One")),
-      new ListTile(title: new Text("Two")),
-    ];
-
     final divided = ListTile
         .divideTiles(
       context: context,
-      tiles: tiles,
+      tiles: _tiles,
     ).toList();
+
+    if (_editMode) {
+      divided.add(new ListTile(
+          title: new TextField(
+            autofocus: true,
+            onSubmitted: _inputSubmitted,
+          ),
+        ),
+      );
+    } else {
+      divided.add(new ListTile(
+          title: new IconButton(
+            icon: new Icon(Icons.add),
+            onPressed: _addItem,
+            color: Colors.blue,
+          ),
+        )
+      );
+    }
 
     return new ListView(children: divided);
   }
 
+  void _addItem() {
+    setState(() {
+      _editMode = true;
+    });
+  }
+
+  void _inputSubmitted(String input) {
+    setState(() {
+      _editMode = false;
+      _tiles.add(createListTile(input));
+    });
+  }
 }

8e2e6e7356d85d52c89d196364d7d97942975785
Tobias Hilka <tobias.hilka@nexusgroup.com>
2018-03-27 10:26:49 +0200

    added add button

--- lib/main.dart
+++ lib/main.dart
@@ -5,21 +5,43 @@ void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
 
-  var _items = [1, 2, 3, 4, 5, 6, 7];
-
   @override
   Widget build(BuildContext context) {
 
     return new MaterialApp(
       title: 'OurList',
-      home: new Scaffold(
+      home: new OurListApp()
+    );
+  }
+}
+
+class OurListApp extends StatefulWidget {
+  @override
+  createState() => new OurListAppState();
+}
+
+class OurListAppState extends State<OurListApp> {
+
+  var _items = [1, 2, 3, 4, 5];
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
         appBar: new AppBar(
           title: new Text('OurList'),
+          actions: <Widget>[
+            new IconButton(icon: new Icon(Icons.add, color: Colors.white,), onPressed: _addItem)
+          ],
         ),
         body: new Center(
           child: new MainList(items: _items),
-        ),
-      ),
+        )
     );
+  }
+
+  void _addItem() {
+    setState(() {
+      _items.add(_items.length + 1);
+    });
   }
 }

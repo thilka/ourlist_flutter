4aaadbf0cd0a4d3f6cd83b8576bd96fc09e8bdbf
Tobias Hilka <tobias.hilka@nexusgroup.com>
2018-03-29 13:03:18 +0200

    Improved scrolling to end of list when keyboard appears. Works on iPhone now, does not work on Android, though.

--- lib/details/detailslist.dart
+++ lib/details/detailslist.dart
@@ -11,10 +11,23 @@ class _DetailsListState extends State<DetailsList> {
   _DetailsListState() {
     _tiles.add(new DetailsItem(text: "One", callback: _dismissItem));
     _tiles.add(new DetailsItem(text: "Two", callback: _dismissItem));
+    _tiles.add(new DetailsItem(text: "Three", callback: _dismissItem));
+    _tiles.add(new DetailsItem(text: "Four", callback: _dismissItem));
+    _tiles.add(new DetailsItem(text: "Five", callback: _dismissItem));
+    _tiles.add(new DetailsItem(text: "Six", callback: _dismissItem));
+    _tiles.add(new DetailsItem(text: "Seven", callback: _dismissItem));
+    _tiles.add(new DetailsItem(text: "Eight", callback: _dismissItem));
+    _tiles.add(new DetailsItem(text: "Nine", callback: _dismissItem));
+    _tiles.add(new DetailsItem(text: "Ten", callback: _dismissItem));
+    _tiles.add(new DetailsItem(text: "Eleven", callback: _dismissItem));
+    _tiles.add(new DetailsItem(text: "Twelve", callback: _dismissItem));
+    _tiles.add(new DetailsItem(text: "Thirteen", callback: _dismissItem));
+    _tiles.add(new DetailsItem(text: "Fourteen", callback: _dismissItem));
+    _tiles.add(new DetailsItem(text: "Fifteen", callback: _dismissItem));
   }
 
   final List<DetailsItem> _tiles = [];
-
+  final ScrollController _scrollController = new ScrollController();
 
   var _editMode = false;
 
@@ -33,7 +46,7 @@ class _DetailsListState extends State<DetailsList> {
       divided.add(createAddButton);
     }
 
-    return new ListView(children: divided);
+    return new ListView(children: divided, controller: _scrollController,);
   }
 
   ListTile get createInputField {
@@ -58,7 +71,17 @@ class _DetailsListState extends State<DetailsList> {
   void _addItem() {
     setState(() {
       _editMode = true;
+      _scrollListToCorrectPositionWhenKeyboardAppears();
     });
+  }
+
+  void _scrollListToCorrectPositionWhenKeyboardAppears() {
+    final approxKeyboardHeight = 300;
+    _scrollController.animateTo(
+      _scrollController.position.maxScrollExtent + approxKeyboardHeight,
+      curve: Curves.easeOut,
+      duration: const Duration(milliseconds: 300),
+    );
   }
 
   void _inputSubmitted(String input) {
